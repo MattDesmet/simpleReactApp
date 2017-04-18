491dc952edd74d92bee33ce2a4c11af077a10ded
Matt Desmet <mattd.email@gmail.com>
2017-04-18 16:18:55 -0700

    updating

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
-console.log(React);
-console.log(ReactDom);
+var myReactElement = React.createElement('h1', {style: {color: 'blue'}}, 'I am a headline');
+
+var actualDOMNode = document.getElementById('app')
+
+ReactDOM.render(myReactElement, actualDOMNode)
